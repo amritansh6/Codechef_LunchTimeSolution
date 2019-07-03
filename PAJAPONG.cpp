9eecba45ccba0a48781b43f168d6e4316ecd49e7
Amritansh Mishra <amritansh40@gmail.com>
2019-07-03 22:36:07 +0530

    Create PAJAPONG.cpp

---
+++ PAJAPONG.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+ 
+#define FAST std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define DECIMAL(n)  std::cout << std::fixed;std::cout << std::setprecision(n);
+#define mod 1000000007
+#define mp make_pair
+#define pb push_back
+#define fi first 
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))  
+#define REVERSE(v) reverse(ALL(v))
+#define endl "\n"
+#define maxc(v) max_element(all(v))
+#define minc(v) min_element(all(v))
+#define GCD(m,n) __gcd(m,n)
+#define debug cerr<<"bhau"<<endl
+#define debugv(x) cerr<<x<<endl
+#define inputarr(a,n) for(int i=0;i<n;++i) cin>>a[i]
+#define printarr(a,n) for(int i=0;i<n;++i) cout<<a[i]<<" "
+#define initarr(a,n,x) for (int i=0;i<n;++i) a[i]=x
+ 
+#define ll long long
+#define ld long double 
+#define pii pair<int, int> 
+#define pll pair<ll, ll>  
+#define vi vector<int>  
+#define vvi vector<vi > 
+#define vii vector<pii >
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x,y,k;
+        cin>>x>>y>>k;
+        ll p=(x+y)/k;
+        if(p%2==0)
+        cout<<"Chef"<<endl;
+        else
+        cout<<"Paja"<<endl;
+       
+    }
+}
